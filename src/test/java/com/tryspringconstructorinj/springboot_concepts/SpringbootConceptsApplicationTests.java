43255b5417031e2814b2503b48ae282146564f34
Kavyashree HM <123004602+kavyashreehm08@users.noreply.github.com>
2024-01-10 22:57:09 +0530

    Spring core project

---
+++ src/test/java/com/tryspringconstructorinj/springboot_concepts/SpringbootConceptsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tryspringconstructorinj.springboot_concepts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootConceptsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
