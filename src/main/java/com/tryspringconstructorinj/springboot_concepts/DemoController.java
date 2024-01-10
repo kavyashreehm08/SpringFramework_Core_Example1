80fdb64bf48f4f28569b851ca8fd148a61df9e0e
Kavyashree HM <123004602+kavyashreehm08@users.noreply.github.com>
2024-01-11 00:31:08 +0530

    Added @Autowire annotation in DemoController.java class

--- src/main/java/com/tryspringconstructorinj/springboot_concepts/DemoController.java
+++ src/main/java/com/tryspringconstructorinj/springboot_concepts/DemoController.java
@@ -1,5 +1,6 @@
 package com.tryspringconstructorinj.springboot_concepts;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     private Coach myCoach;
-
+@Autowired
     public DemoController(Coach theCoach) {
         myCoach = theCoach;
     }
